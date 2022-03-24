87e4f86638130bb0bfd0e5af8eb4fed26e63b5bf
Joel Pinto <joelpostman97@gmail.com>
2022-03-24 15:50:22 +0000

    audio-player: Add bus monitorization
    This commit implements gst bus monitorization and callback registering
    for end-of-stream and error messages.
    
    Signed-off-by: Joel Pinto <joelpostman97@gmail.com>

--- audio-player.c
+++ audio-player.c
@@ -17,10 +17,14 @@ typedef struct _CustomData
   GMainLoop *loop;
 } CustomData;
 
+static void eos_callback (GstBus *, GstMessage *, CustomData *);
+static void error_callback (GstBus *, GstMessage *, CustomData *);
+
 int main (int argc, char *argv[])
 {
   CustomData data;
   GstStateChangeReturn ret;
+  GstBus *bus;
   gint flags;
 
   /* Initialize GStreamer */
@@ -40,6 +44,12 @@ int main (int argc, char *argv[])
   flags &= ~GST_PLAY_FLAG_VIDEO & ~GST_PLAY_FLAG_TEXT;
   g_object_set (data.playbin, "flags", flags, NULL);
 
+  bus = gst_element_get_bus (data.playbin);
+  gst_bus_add_signal_watch (bus);
+  g_signal_connect (G_OBJECT (bus), "message::error", (GCallback) error_callback, &data);
+  g_signal_connect (G_OBJECT (bus), "message::eos", (GCallback) eos_callback, &data);
+  gst_object_unref(bus);
+
   ret = gst_element_set_state (data.playbin, GST_STATE_PLAYING);
   if (ret == GST_STATE_CHANGE_FAILURE) {
     g_printerr ("Unable to set the pipeline to the playing state.\n");
@@ -56,4 +66,23 @@ int main (int argc, char *argv[])
   gst_element_set_state (data.playbin, GST_STATE_NULL);
   gst_object_unref (data.playbin);
   return 0;
+}
+
+static void eos_callback (GstBus *bus, GstMessage *msg, CustomData *data) {
+  g_print("EOS\n");
+}
+
+static void error_callback (GstBus *bus, GstMessage *msg, CustomData *data) {
+  GstStateChangeReturn ret;
+  GError *error;
+
+  gst_message_parse_error(msg, &error, NULL);
+  g_printerr("Error from component %s, error %s\n", GST_OBJECT_NAME(msg->src), error->message);
+  g_error_free(error);
+
+  ret = gst_element_set_state (data->playbin, GST_STATE_READY);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_printerr ("Unable to set the pipeline to the ready state.\n");
+    gst_object_unref (data->playbin);
+  }
 }
